ab4982854a531ae59aadfe90cb804ce8404e957d
tieppt07 <tieppt07@gmail.com>
2015-12-21 09:56:14 +0700

    Migrate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,21 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->index();
             $table->string('email')->unique();
             $table->string('password', 60);
+            $table->integer('role')->default(0);
+            $table->integer('position_id')->unsigned()->index();
+            $table->integer('department_id')->unsigned()->index();
+            $table->integer('status')->default(1);
+            $table->string('place_of_birth')->nullable();
+            $table->date('birthday')->nullable();
+            $table->string('phone')->index();
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('position_id')->references('id')->on('positions');
+            $table->foreign('department_id')->references('id')->on('departments');
         });
     }
 
